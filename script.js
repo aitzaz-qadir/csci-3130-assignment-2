aea4b772c453e6ddb8bd7c8660198de6f4143167
akhowaja <at382154@dal.ca>
2021-06-08 17:44:50 -0300

    Added Assignment-2 Files

---
+++ script.js
@@ -0,0 +1,24 @@
+// Setting DOM circles into variables.
+var circle1 = document.getElementsByClassName("circle-1");
+var circle2 = document.getElementsByClassName("circle-2");
+var circle3 = document.getElementById("transparent");
+// Assigning text tags to variables.
+var p1 = document.getElementById("para-1");
+var p2 = document.getElementById("para-2");
+// Assigning button tag to a variable.
+var button = document.getElementsByTagName("button");
+
+// Function for the intersection (triggered by button).
+function intersect() {
+	// Moving the circles towards each other.
+	circle1[0].style.marginLeft = "25%";
+	circle3.style.marginLeft    = "25%";
+	circle2[0].style.marginRight= "25%";
+	// Removing one para element.
+	p2.remove();
+	// Changing text on the first para.
+	p1.innerHTML = "Intersection";
+	p1.style.paddingLeft = "5%";
+	// Hiding the button as instructed.
+	button[0].style.display = "none";
+}
