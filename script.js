6f3cfa7de18fd04e46e18661c53ddc20482db43b
Aitzaz Qadir Khowaja <at382154@dal.ca>
2021-06-12 19:41:32 -0300

    Update comments script.js

--- script.js
+++ script.js
@@ -2,23 +2,28 @@
 var circle1 = document.getElementsByClassName("circle-1");
 var circle2 = document.getElementsByClassName("circle-2");
 var circle3 = document.getElementById("transparent");
+
 // Assigning text tags to variables.
 var p1 = document.getElementById("para-1");
 var p2 = document.getElementById("para-2");
+
 // Assigning button tag to a variable.
 var button = document.getElementsByTagName("button");
 
-// Function for the intersection (triggered by button).
+// Function for the intersection (triggered by html button).
 function intersect() {
 	// Moving the circles towards each other.
 	circle1[0].style.marginLeft = "25%";
 	circle3.style.marginLeft    = "25%";
 	circle2[0].style.marginRight= "25%";
+	
 	// Removing one para element.
 	p2.remove();
+	
 	// Changing text on the first para.
 	p1.innerHTML = "Intersection";
 	p1.style.paddingLeft = "5%";
+	
 	// Hiding the button as instructed.
 	button[0].style.display = "none";
 }
